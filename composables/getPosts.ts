28c2692b654f3a542d921059f0178b05a91c34bf
Renato Lacerda <renato.ac.lacerda@gmail.com>
2023-04-18 11:02:35 -0300

    fix: using useAsyncData to fix query problems

--- composables/getPosts.ts
+++ composables/getPosts.ts
@@ -3,25 +3,28 @@ export default async function (
   tag?: string,
   limit?: number
 ) {
-  const query = queryContent(`/posts/`).only([
-    "title",
-    "tags",
-    "publishDate",
-    "shortDescription",
-    "_path",
-  ]);
+  const { data } = await useAsyncData(() => {
+    const query = queryContent(`/posts/`).only([
+      "title",
+      "tags",
+      "publishDate",
+      "shortDescription",
+      "_path",
+    ]);
 
-  if (excludeDraft) {
-    query.where({ draft: false });
-  }
+    if (excludeDraft) {
+      query.where({ draft: false });
+    }
 
-  if (tag) {
-    query.where({ tags: { $contains: tag } });
-  }
+    if (tag) {
+      query.where({ tags: { $contains: tag } });
+    }
 
-  if (limit) {
-    query.limit(5);
-  }
+    if (limit) {
+      query.limit(5);
+    }
 
-  return query.find();
+    return query.find();
+  });
+  return data;
 }
