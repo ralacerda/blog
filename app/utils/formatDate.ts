1d49165f57359d9aa758d19436097abd032b69f8
Renato Lacerda <renatolacerda@pecege.com>
2024-06-20 13:06:00 -0300

    fix: using Intl.DateTimeFormat

--- app/utils/formatDate.ts
+++ app/utils/formatDate.ts
@@ -1,10 +1,10 @@
 export default function (date: Date, locale: string) {
   const dateLocale = locale === "en" ? "en-us" : "pt-br";
 
-  return new Date(date).toLocaleDateString(dateLocale, {
+  return new Intl.DateTimeFormat(dateLocale, {
     year: "numeric",
     month: "long",
     day: "numeric",
     timeZone: "UTC",
-  });
+  }).format(new Date(date));
 }
